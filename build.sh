0b0dfba4b907ec31f1cf5c82cb0d229fbd8d422b
LAK132 <lakridesagain@gmail.com>
2023-09-23 15:07:01 +0930

    reformat build script

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #! /bin/sh
 mkdir -p build
-pandoc --from=latex --to=html --standalone --output=build/index.html --bibliography=bibliography.bib main.tex
+pandoc \
+  --from=latex \
+  --to=html \
+  --standalone \
+  --output=build/index.html \
+  --bibliography=bibliography.bib \
+  main.tex
